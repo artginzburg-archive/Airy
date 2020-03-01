bf4be819e89f60a1793a7dca33b9035847d51bc8
DaFuqtor <46137336+DaFuqtor@users.noreply.github.com>
2020-03-01 20:53:29 +0300

    battery indication (bad)

--- Airy/StatusMenuController.swift
+++ Airy/StatusMenuController.swift
@@ -16,6 +16,8 @@ class StatusMenuController: NSObject, NSMenuDelegate, BluetoothConnectorListener
   
   @IBOutlet weak var statusMenu: NSMenu!
   
+  @IBOutlet weak var batteryStatusButton: NSMenuItem!
+  
   override func awakeFromNib() {
     statusItem.menu = statusMenu
     statusMenu.delegate = self
@@ -53,16 +55,46 @@ class StatusMenuController: NSObject, NSMenuDelegate, BluetoothConnectorListener
     guard let button = statusItem.button else { return }
     
     if bluetooth.isConnected {
+      
       button.image = NSImage(named: "icon-inverted")
       action = {
         self.disconnect(button)
       }
       button.toolTip = bluetooth.bluetoothDevice.name
+      
+      let commandLeft = "defaults read /Library/Preferences/com.apple.Bluetooth    | grep BatteryPercentLeft | tr -d \\; | awk '{print $3}'"
+      let commandRight = "defaults read /Library/Preferences/com.apple.Bluetooth    | grep BatteryPercentRight | tr -d \\; | awk '{print $3}'"
+      let commandCase = "defaults read /Library/Preferences/com.apple.Bluetooth    | grep BatteryPercentCase | tr -d \\; | awk '{print $3}'"
+      
+      var batteryString : String = ""
+      
+      let leftBattery = shell(commandLeft)
+      if leftBattery != "0" {
+        batteryString.append("L: \(leftBattery)%")
+      }
+      
+      let rightBattery = shell(commandRight)
+      if rightBattery != "0" {
+       batteryString.append("  R: \(rightBattery)%")
+      }
+      
+      let caseBattery = shell(commandCase)
+      if caseBattery != "0" {
+        batteryString.append("  C: \(caseBattery)%")
+      }
+      
+      if !batteryString.isEmpty {
+        batteryStatusButton.isHidden = false
+        batteryStatusButton.title = batteryString
+      }
+      
     } else {
       button.image = NSImage(named: "icon")
       action = {
         self.connect(button)
       }
+      button.toolTip = ""
+      batteryStatusButton.isHidden = true
     }
   }
   
@@ -82,4 +114,19 @@ class StatusMenuController: NSObject, NSMenuDelegate, BluetoothConnectorListener
     bluetooth.disconnect()
   }
   
+}
+
+@discardableResult
+func shell(_ command: String) -> String {
+  let task = Process()
+  task.launchPath = "/bin/bash"
+  task.arguments = ["-c", command]
+  
+  let pipe = Pipe()
+  task.standardOutput = pipe
+  task.launch()
+  
+  let data = pipe.fileHandleForReading.readDataToEndOfFile()
+  
+  return NSString(data: data, encoding: String.Encoding.utf8.rawValue)! as String
 }
